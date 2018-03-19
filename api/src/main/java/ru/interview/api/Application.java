1603bcf97c4a12ef6fb93fabccdbeef076cf697e
Andrey Erokhin <andrey.erokhin@here.com>
2018-03-19 03:03:33 +0100

    Fix typos and imports

--- api/src/main/java/ru/interview/api/Application.java
+++ api/src/main/java/ru/interview/api/Application.java
@@ -1,6 +1,5 @@
 package ru.interview.api;
 
-import com.google.common.base.Predicate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
